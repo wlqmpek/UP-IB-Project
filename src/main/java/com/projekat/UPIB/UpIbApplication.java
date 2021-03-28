c5268cc5b6f7f3d00e2a026d69e53185c8d00c08
markoacimovic <acimovic.sf46.2019@uns.ac.rs>
2021-03-28 17:34:09 +0200

    init commit

---
+++ src/main/java/com/projekat/UPIB/UpIbApplication.java
@@ -0,0 +1,13 @@
+package com.projekat.UPIB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpIbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpIbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projekat/UPIB/UpIbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekat.UPIB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpIbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
