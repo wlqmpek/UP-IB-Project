cd230b9cd5176224569e7e2ae764eb40557e1356
wlqmpek <milosvlku@hotmail.com>
2021-03-30 10:58:32 +0200

    Dodati repozitorijumi

---
+++ src/main/java/com/projekat/UPIB/repositories/MedicinskaSestraRepozitorijum.java
@@ -0,0 +1,6 @@
+package com.projekat.UPIB.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicinskaSestraRepozitorijum  extends JpaRepository<MedicinskaSestra, Long> {
+}
