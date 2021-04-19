7cbd83dea03ca5bd14b90767f87cf685110e7ed0
markoacimovic <acimovic.sf46.2019@uns.ac.rs>
2021-04-19 20:39:37 +0200

    Zavrsen kontroler za zahteve korisnika za registraciju

---
+++ src/main/java/com/projekat/UPIB/controllers/ZahteviZaRegistracijuController.java
@@ -0,0 +1,48 @@
+package com.projekat.UPIB.controllers;
+
+import com.projekat.UPIB.enums.StatusKorisnika;
+import com.projekat.UPIB.models.Pacijent;
+import com.projekat.UPIB.services.implementation.PacijentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/Zahtevi")
+public class ZahteviZaRegistracijuController {
+
+    @Autowired
+    private PacijentService pacijentService;
+
+    @GetMapping
+    public ResponseEntity<List<Pacijent>> getPacijents(){
+        List<Pacijent> pacijenti = pacijentService.findAll();
+        List<Pacijent> retVal = new ArrayList<>();
+
+        for (Pacijent pacijent : pacijenti) {
+            if(pacijent.getStatusKorisnika().equals(StatusKorisnika.NA_CEKANJU)){
+                retVal.add(pacijent);
+            }
+        }
+
+        return  new ResponseEntity<>(retVal, HttpStatus.OK);
+    }
+
+    @PutMapping(consumes = "application/json", value = "/{id}")
+    public ResponseEntity<Pacijent> getPacijent(@PathVariable("id") long id, @RequestBody Pacijent pacijent){
+
+        Pacijent retVal = pacijentService.findOne(id);
+        if(retVal == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        retVal.setStatusKorisnika(pacijent.getStatusKorisnika());
+        retVal = pacijentService.save(retVal);
+
+        return new ResponseEntity<>(retVal, HttpStatus.ACCEPTED);
+    }
+}
--- src/main/java/com/projekat/UPIB/enums/StatusKorisnika.java
+++ src/main/java/com/projekat/UPIB/enums/StatusKorisnika.java
@@ -2,6 +2,7 @@ package com.projekat.UPIB.enums;
 
 public enum StatusKorisnika {
     PRIHVACEN,
+    NA_CEKANJU,
     ODBIJEN,
     BLOKIRAN
 }
