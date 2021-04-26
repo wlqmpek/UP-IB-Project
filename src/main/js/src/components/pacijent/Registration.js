5423b854dc66748cad4679ed51f0c98e5889b198
Marko Aćimović <m.acimovic001@gmail.com>
2021-04-26 09:22:06 +0200

    dodate validacije za registraciju

--- src/main/js/src/components/pacijent/Registration.js
+++ src/main/js/src/components/pacijent/Registration.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import validator from 'validator'
 
 const Registration = () => {
 
@@ -6,6 +7,7 @@ const Registration = () => {
     const [prezimeKorisnika, setPrezime] = useState('')
     const [emailKorisnika, setEmail] = useState('')
     const [lozinkaKorisnika, setLozinka] = useState('')
+    const [ponovljenaLozinka, setPonovljena] = useState('')
     const [jbzo, setJbzo] = useState('')
     const [pacijenti, setPacijenti] = useState([])
 
@@ -40,8 +42,23 @@ const Registration = () => {
     const onSubmit = (e) =>{
         e.preventDefault();
 
-        if(!imeKorisnika || !prezimeKorisnika || !emailKorisnika || !lozinkaKorisnika || !jbzo ){
+        if(!imeKorisnika || !prezimeKorisnika || !emailKorisnika || !lozinkaKorisnika || !jbzo || !ponovljenaLozinka){
             alert('Sva polja su obavezna.')
+            return
+        }
+
+        if(lozinkaKorisnika.valueOf() != ponovljenaLozinka.valueOf()){
+            alert('Ponovljena lozinka nije ista kao uneta lozinka')
+            return
+        }
+
+        if(isNaN(jbzo)){
+            alert('Jbzo mora sadrzat samo cifre')
+            return
+        }
+
+        if(!validator.isEmail(emailKorisnika)){
+            alert('Email nije validan')
             return
         }
         PostPacijent(imeKorisnika, prezimeKorisnika, emailKorisnika, lozinkaKorisnika, jbzo)
@@ -50,6 +67,7 @@ const Registration = () => {
         setPrezime('')
         setEmail('')
         setLozinka('')
+        setPonovljena('')
         setJbzo('')
     }
 
@@ -75,6 +93,10 @@ const Registration = () => {
                             <div className='form-group'>
                                 <label>Lozinka: </label>
                                 <input type='password' value={lozinkaKorisnika} onChange={e => setLozinka(e.target.value)} placeholder='Lozinka' className='form-control' />
+                            </div>
+                            <div className='form-group'>
+                                <label>Ponovljena lozinka: </label>
+                                <input type='password' value={ponovljenaLozinka} onChange={e => setPonovljena(e.target.value)} placeholder='Ponovljena lozinka' className='form-control' />
                             </div>
                             <div className='form-group'>
                                 <label>JBZO: </label>
